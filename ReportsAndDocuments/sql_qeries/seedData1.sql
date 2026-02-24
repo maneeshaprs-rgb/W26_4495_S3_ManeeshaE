158d1736d36309e1f4d92d91ce25bffb3745ec68
maneeshaprs-rgb <maneeshaprs@gmail.com>
2026-02-23 19:18:54 -0800

    updates

--- ReportsAndDocuments/sql_qeries/seedData1.sql
+++ ReportsAndDocuments/sql_qeries/seedData1.sql
@@ -1,11 +1,19 @@
 USE FarmVendorDb;
 GO
 
+SELECT COUNT(*) AS Email FROM AspNetUsers;
 SELECT COUNT(*) AS Products FROM Product;
+SELECT COUNT(*) AS InventoryLots FROM InventoryLot;
 SELECT COUNT(*) AS DemandRequests FROM DemandRequest;
 SELECT COUNT(*) AS Dispatches FROM Dispatch;
-SELECT COUNT(*) AS InventoryLots FROM InventoryLot;
 
 SELECT TOP 10 Email, UserName FROM AspNetUsers;
+SELECT TOP 10 * FROM AspNetUsers;
+SELECT TOP 10 * FROM Product;
 
-SELECT * FROM AspNetUsers;
+SELECT TOP 10 * FROM InventoryLot ORDER BY CreatedAt DESC;
+
+SELECT TOP 10 * FROM DemandRequest ORDER BY NeededBy DESC;
+
+SELECT TOP 10 * FROM Dispatch ORDER BY DispatchId DESC;
+
