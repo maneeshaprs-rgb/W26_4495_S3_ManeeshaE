b9ba7551eea03e9ee6916f5c6cf11c6713ee6e63
maneeshaprs-rgb <maneeshaprs@gmail.com>
2026-03-18 12:31:35 -0700

    chatModule

---
+++ implementations/FarmVendor/FarmVendor.Api/Migrations/20260318192209_AddChatModul.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FarmVendor.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChatModul : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
